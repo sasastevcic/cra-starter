8e4f0f5e522b5a0a15341866ee5532ab576c944b
Sasa Stevcic <sasa.stevcic@yahoo.com>
2022-04-14 15:28:20 +0200

    Add createStore util

---
+++ src/utils/createStore.tsx
@@ -0,0 +1,35 @@
+import { createContext, FunctionComponent, PropsWithChildren, Provider, useContext } from 'react';
+
+/**
+ * Creates a context for the store created by the passed `store`.
+ * Additionally returns the `Provider` component and the hook for the created store's context.
+ *
+ * @param name - name of the store
+ * @param store - a function that returns an object representing the store
+ */
+export const createStore = <TStore extends Record<string, unknown>>(
+	name: string,
+	store: () => TStore,
+): [FunctionComponent<Provider<TStore>>, () => TStore] => {
+	const StoreContext = createContext<TStore>({} as TStore);
+
+	const StoreProvider = ({ children }: PropsWithChildren<unknown>): JSX.Element => {
+		const storeValue = store();
+
+		return <StoreContext.Provider value={storeValue}>{children}</StoreContext.Provider>;
+	};
+
+	const useStore = (): TStore => {
+		const store = useContext(StoreContext);
+
+		if (Object.keys(store).length > 0) {
+			return store;
+		}
+
+		throw new ReferenceError(
+			`${name} is not initialized, make sure that your component is wrapped in the 'Provider' of the store.`,
+		);
+	};
+
+	return [StoreProvider, useStore];
+};
